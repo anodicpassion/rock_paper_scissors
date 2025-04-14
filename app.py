3de579439a36f7b586228b21f180e2c9a9b5cebc
anodicpassion <pratikpawarkarad@gmail.com>
2025-04-15 01:19:55 +0530

    app: updated socket

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ socketio = SocketIO(app)
 players = []
 choices = {}
 restart_votes = set()
+has_game_end = "0"
 
 @app.route('/')
 def index():
@@ -23,21 +24,35 @@ def handle_join():
     join_room("game")
     emit('waiting', broadcast=True)
     if len(players) == 2:
+        # emit('start', broadcast=True)
+    
+        
         emit('start', broadcast=True)
+    
 
 @socketio.on('choice')
 def handle_choice(data):
+    # global score
     choices[request.sid] = data['choice']
     if len(choices) == 2:
         sid1, sid2 = list(choices.keys())
         result = get_result(choices[sid1], choices[sid2])
+       
+        
         socketio.emit('result', {
             sid1: result[0],
             sid2: result[1],
             'choice1': choices[sid1],
-            'choice2': choices[sid2]
+            'choice2': choices[sid2],
+            'has_game_end': has_game_end
         }, room="game")
         choices.clear()
+
+@socketio.on("game_end")
+def game_end(arg):
+    emit('loose', broadcast=True)
+    # socketio.emit('loose', room="game")
+    
 
 @socketio.on('restart')
 def handle_restart():
@@ -45,6 +60,13 @@ def handle_restart():
     if len(restart_votes) == 2:
         restart_votes.clear()
         socketio.emit('start', room="game")
+
+@socketio.on('soft_restart')
+def handle_restart():
+    restart_votes.add(request.sid)
+    if len(restart_votes) == 2:
+        restart_votes.clear()
+        socketio.emit('soft_start', room="game")
 
 def get_result(choice1, choice2):
     if choice1 == choice2:
@@ -57,6 +79,8 @@ def get_result(choice1, choice2):
 
 @socketio.on('disconnect')
 def handle_disconnect():
+    # global score
+    score = {}
     if request.sid in players:
         players.remove(request.sid)
     if request.sid in choices:
